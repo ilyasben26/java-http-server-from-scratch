f03bdb6baacd14fbd632fccf57821aef366e9a59
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-03-05 15:20:14 +0100

    opening a port, listening for TCP requests on it and then sending a predefined response

--- src/main/java/com/ilyasben/httpserver/HttpServer.java
+++ src/main/java/com/ilyasben/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.ilyasben.httpserver;
 import com.ilyasben.httpserver.config.Configuration;
 import com.ilyasben.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * Driver Class for the Http Server
@@ -20,5 +26,40 @@ public class HttpServer {
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using WebRoot: " + conf.getWebroot());
+
+        // TCP
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept(); // waits for a connection
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // TODO: reading request
+
+            // writing response
+            String html = "<html><head><title>Simple HTTP Server</title><body><h1>Hello, World!</h1></body></head></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + // HEADER
+                    CRLF + // extra CRLF to say that header is done
+                    html + CRLF + // HTML
+                    CRLF ; // extra CRLF to say that HTML is done
+
+            // sending the response
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

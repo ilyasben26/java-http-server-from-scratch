07a888cdf2d90a3e857e4f7571cedfbb652a01e8
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-03-05 15:05:39 +0100

    loading and initializing the configuration in main

--- src/main/java/com/ilyasben/httpserver/HttpServer.java
+++ src/main/java/com/ilyasben/httpserver/HttpServer.java
@@ -1,5 +1,6 @@
 package com.ilyasben.httpserver;
 
+import com.ilyasben.httpserver.config.Configuration;
 import com.ilyasben.httpserver.config.ConfigurationManager;
 
 /**
@@ -12,6 +13,12 @@ public class HttpServer {
 
         System.out.println("Server starting...");
 
+        // using the Configuration Manager singleton to load a configuration file
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
+        // fetching the loaded configuration
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using WebRoot: " + conf.getWebroot());
     }
 }
